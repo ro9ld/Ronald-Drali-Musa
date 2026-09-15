f2c5954c6ad765d346a73c91f6b450a7e98813bc
ro9ld <info@ronaldmusa.com>
2026-09-15 21:12:58 +1000

    Delete ronaldmusa directory

--- ronaldmusa/lib/contact.ts
+++
@@ -1,4 +0,0 @@
-export const INSTAGRAM_HANDLE = "ro9ld";
-export const INSTAGRAM_URL = `https://instagram.com/${INSTAGRAM_HANDLE}`;
-export const EMAIL = "info@ronaldmusa.com";
-export const MAILTO_URL = `mailto:${EMAIL}`;
