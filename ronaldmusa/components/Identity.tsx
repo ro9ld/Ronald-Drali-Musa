19948ee777af7a4eba6a783aed65e07b5c162d3c
ro9ld <info@ronaldmusa.com>
2026-09-15 21:58:57 +1000

    Delete ronaldmusa directory

--- ronaldmusa/components/Identity.tsx
+++
@@ -1,9 +0,0 @@
-export default function Identity() {
-  return (
-    <section className="pt-[26px] pb-[22px] sm:pt-10 sm:pb-7">
-      <p className="text-[17px] font-medium tracking-tight sm:text-[19px]">
-        Videography · Photography
-      </p>
-    </section>
-  );
-}
